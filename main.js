5f0dc08928cca242a1e32d286b2a4933222ffbfa
hsnbera <hasanbera.kabadayi@serebellum.com>
2025-09-09 23:48:00 +0300

    feat: add orthographic camera and update rendering logic

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import * as THREE from 'three';
+import { color } from 'three/tsl';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75, window.innerWidth / window.innerHeight,
   0.1, 1000 );
+
+const camera_s = new THREE.OrthographicCamera(75, window.innerWidth / window.innerHeight,
+  0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth,
@@ -13,19 +17,19 @@ renderer.setAnimationLoop (animate);
 document.body.appendChild(
   renderer.domElement
 );
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-  color:  0x00ff00
-});
-const cube = new THREE.Mesh( geometry, material);
-scene.add( cube );
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color:0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
 
 camera.position.z = 5;
+camera_s.position.z = 10;
 
 function animate() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
   renderer.render( scene, camera);
+  setTimeout(() => {
+    renderer.render(scene, camera_s);
+  }, 10000);
 }
