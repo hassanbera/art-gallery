7952826d373b8207e30d31be7a5be6dd9a8f55f0
hsnbera <hasanbera.kabadayi@serebellum.com>
2025-09-13 00:03:05 +0300

    refactor: standardize string quotes and improve code formatting in main.js

--- main.js
+++ main.js
@@ -1,60 +1,63 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 const images = [
-  'socrates.jpg',
-  'stars.jpg',
-  'wave.jpg',
-  'spring.jpg',
-  'mountain.jpg',
-  'sunday.jpg'
+  "socrates.jpg",
+  "stars.jpg",
+  "wave.jpg",
+  "spring.jpg",
+  "mountain.jpg",
+  "sunday.jpg",
 ];
 
 const titles = [
-  'The Death of Socrates',
-  'Starry Night',
-  'The Great Wave off Kanagawa',
-  'Effect of Spring, Giverny',
-  'Mount Corcoran',
-  'A Sunday on La Grande Jatte'
+  "The Death of Socrates",
+  "Starry Night",
+  "The Great Wave off Kanagawa",
+  "Effect of Spring, Giverny",
+  "Mount Corcoran",
+  "A Sunday on La Grande Jatte",
 ];
 
 const artists = [
-  'Jacques-Louis David',
-  'Vincent Van Gogh',
-  'Katsushika Hokusai',
-  'Claude Monet',
-  'Albert Bierstadt',
-  'George Seurat'
+  "Jacques-Louis David",
+  "Vincent Van Gogh",
+  "Katsushika Hokusai",
+  "Claude Monet",
+  "Albert Bierstadt",
+  "George Seurat",
 ];
 
-
-
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const baseNode = new THREE.Object3D();
+scene.add(baseNode);
 
-camera.position.z = 5;
 
+const artwork = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 2, 0.1),
+  new THREE.MeshBasicMaterial({ color: 0xf08080 })
+);
+
+baseNode.add(artwork);
+
+artwork.position.z = -4;
 function animate() {
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render( scene, camera );
-
+  renderer.render(scene, camera);
 }
 
-window.addEventListener('resize', ()=> {
+window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-})
+});
