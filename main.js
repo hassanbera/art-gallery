4e4ec7d7ef130f32bc4140dd1c2d411b02bc2b44
hsnbera <hasanbera.kabadayi@serebellum.com>
2025-09-14 21:18:00 +0300

    feat: add Reflector for mirror effect and integrate arrow navigation in artwork display

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { Reflector } from "three/examples/jsm/Addons.js";
 
 const images = [
   "socrates.jpg",
@@ -24,6 +25,8 @@ document.body.appendChild(renderer.domElement);
 const rootNode = new THREE.Object3D();
 scene.add(rootNode);
 
+const leftArrowImage = textureLoader.load(`left.png`);
+const rightArrowImage = textureLoader.load(`right.png`);
 
 const count = images.length;
 for (let i = 0; i < count; i++) {
@@ -46,6 +49,20 @@ for (let i = 0; i < count; i++) {
   artwork.position.z = -4;
   baseNode.add(artwork);
 
+  const leftArrow = new THREE.Mesh(
+    new THREE.BoxGeometry(0.3, 0.3, 0.01),
+    new THREE.MeshStandardMaterial({map: leftArrowImage, transparent:true})
+  )
+  leftArrow.position.set(2.9, 0, -4);
+  baseNode.add(leftArrow);
+
+  const rightArrow = new THREE.Mesh(
+    new THREE.BoxGeometry(0.3, 0.3, 0.01),
+    new THREE.MeshStandardMaterial({map: rightArrowImage, transparent:true})
+  )
+  rightArrow.position.set(-2.9,0,-4);
+  baseNode.add(rightArrow);
+
   rootNode.add(baseNode);
 }
 
@@ -55,6 +72,18 @@ spotlight.target.position.set(0, 1, -5);
 scene.add(spotlight);
 scene.add(spotlight.target);
 
+const mirror = new Reflector (
+  new THREE.CircleGeometry(40,64),
+  {
+    color: 0x505050,
+    textureWidth: window.innerWidth * window.devicePixelRatio,
+    textureHeight: window.innerHeight * window.devicePixelRatio,
+  }
+);
+
+mirror.position.set(0, -1.1, 0);
+mirror.rotateX(-Math.PI / 2);
+scene.add(mirror);
 
 function animate() {
   rootNode.rotation.y += 0.001;
